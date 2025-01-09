4022e595da64be24ded2be8f60988967978d5e7e
shamirHere <aslanshamir@gmail.com>
2025-01-09 17:07:24 +0530

    basic folder and navigation setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Welcome } from './src/screens/auth'
+
+const App = () => {
+  return (
+    <View>
+      <Welcome />
+    </View>
+  )
+}
+
+export default App; 
